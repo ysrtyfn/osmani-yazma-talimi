eb5119479ad3414c8c3c2a08879d4547853f8dc5
Tayfun Yasar <ysrtayfun@gmail.com>
2023-12-30 18:33:40 +0300

    ibtidai kodlar

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Osmani Yazma Talimi",
+  description:
+    "Osmanlı Türkçesi ile yazmayı talim etmek için sahife hazırlamağa yarar programdır.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
