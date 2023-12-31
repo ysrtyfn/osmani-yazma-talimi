617d7d939030ce18313a0c2a203262f946161994
Tayfun Yasar <ysrtayfun@gmail.com>
2023-12-31 22:11:04 +0300

    Takdim kısmı tertiplendi.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/unsurlar/globals.css";
+import { cn } from "@/nuve/aletler/tasnifci";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "min-h-screen")}>{children}</body>
     </html>
   );
 }
